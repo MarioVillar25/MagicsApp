abb04a37a492266d1770096d0bda08bf1f8d9cf6
mariovillar <mario.villar@nter.es>
2024-07-02 17:44:20 +0200

    enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/cards-list/cards-list.component').then(
+            (c) => c.CardsListComponent
+          ),
+      },
+      {
+        path: 'card-page/:id',
+        loadComponent: () =>
+          import('./pages/card-page/card-page.component').then(
+            (c) => c.CardPageComponent
+          ),
+      },
+      {
+        path: 'error-page',
+        loadComponent: () =>
+          import('./pages/error-page/error-page.component').then(
+            (c) => c.ErrorPageComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'error-page',
+      },
+    ],
+  },
+];
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../components/footer/footer.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
